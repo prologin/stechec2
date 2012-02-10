cd36d03e966d2032fc0041104a59b5dc5c7a23fd
Nicolas Hureau <nicolas@lse.epita.fr>
2012-02-10 09:27:08 +0100

    server: You can now pass a config.ini file to the server

--- src/server/options.cc
+++ src/server/options.cc
@@ -1,6 +1,7 @@
 #include "options.hh"
 
 #include <iostream>
+#include <fstream>
 #include <exception>
 #include <boost/program_options.hpp>
 
@@ -11,37 +12,49 @@ void Options::process(int argc, char** argv)
     std::string usage =
         std::string("Usage: ") + argv[0] + " [options]\n";
 
-    po::options_description cli_opt("CLI options");
+    po::options_description cli_opt("Command-line options");
     cli_opt.add_options()
         ("help,h", "Show this help")
-        ("version,v", "Show version");
+        ("version,v", "Show version")
+        ("config,c", po::value<std::string>(&config),
+                        "Set configuration file to use");
 
-    po::options_description config_opt("Config options");
-    config_opt.add_options()
-        ("config,c", po::value<std::string>(&config)->default_value(""),
-                        "Set configuration file to use")
-        ("pub_addr,p",
-            po::value<std::string>(&pub_addr)->default_value("tcp://*:4242"),
-                        "Set publishing address binding (ZeroMQ style)")
-        ("rep_addr,r",
-            po::value<std::string>(&rep_addr)->default_value("tcp://*:4243"),
-                        "Set reply address binding (ZeroMQ style)")
-        ("nb_spectator,s",
+    po::options_description config_cli_opt("Config options");
+    config_cli_opt.add_options()
+        ("rep_addr,r,server.rep_addr",
+            po::value<std::string>(&rep_addr)->default_value("tcp://*:4242"),
+                        "Set reply address binding (ZeroMQ)")
+        ("pub_addr,p,server.pub_addr",
+            po::value<std::string>(&pub_addr)->default_value("tcp://*:4243"),
+                        "Set publishing address binding (ZeroMQ)")
+        ("nb_spectator,s,server.nb_spectator",
             po::value<unsigned>(&nb_spectator)->default_value(0),
                         "Number of spectator to expect")
-        ("wait_timeout,w",
+        ("wait_timeout,w,server.wait_timeout",
             po::value<unsigned>(&wait_timeout)->default_value(1),
                         "Timeout for a player (in milliseconds)")
-        ("log,o", po::value<std::string>(&log)->default_value("stechec2.log"),
+        ("log,o,server.log",
+            po::value<std::string>(&log)->default_value("stechec2.log"),
                         "Output file in which to save the log")
-        ("verbose,d", po::value<unsigned>(&verbose)->default_value(3),
-                        "Verbosity of the log (0-3)")
-        ("start_game_timeout,g",
+        ("verbose,d,server.verbose",
+            po::value<unsigned>(&verbose)->default_value(5),
+                        "Verbosity of the log (0-5)")
+        ("start_game_timeout,g,server.start_game_timeout",
             po::value<unsigned>(&start_game_timeout)->default_value(5),
                         "Timeout before the game begins (in milliseconds)");
 
-    po::options_description opt("All options");
-    opt.add(cli_opt).add(config_opt);
+    po::options_description config_opt("Config options");
+    config_opt.add_options()
+        ("server.rep_addr", po::value<std::string>(&rep_addr))
+        ("server.pub_addr", po::value<std::string>(&pub_addr))
+        ("server.nb_spectator", po::value<unsigned>(&nb_spectator))
+        ("server.wait_timeout", po::value<unsigned>(&wait_timeout))
+        ("server.log", po::value<std::string>(&log))
+        ("server.verbose", po::value<unsigned>(&verbose))
+        ("server.start_game_timeout", po::value<unsigned>(&start_game_timeout));
+
+    po::options_description opt("");
+    opt.add(cli_opt).add(config_cli_opt);
 
     try
     {
@@ -65,10 +78,21 @@ void Options::process(int argc, char** argv)
                       << std::endl;
             exit(0);
         }
+
+        if (vm.count("config"))
+        {
+            std::ifstream input_file(config);
+
+            if (!input_file.is_open())
+                throw std::runtime_error("Could not open config file \"" +
+                        config + "\"");
+            po::store(po::parse_config_file(input_file, config_opt, true), vm);
+            po::notify(vm);
+        }
     }
     catch (const std::exception& e)
     {
-        std::cerr << e.what() << '\n' << usage << opt << std::endl;
+        std::cerr << e.what() << "\n\n" << usage << opt << std::endl;
         exit(1);
     }
 }
