3f2082a1b8fbc8eee88fa455a1a945ebbf421704
Nicolas Hureau <nh@kalenz.fr>
2012-04-07 21:45:10 +0200

    tictactoe: Add dummy server_loop

--- games/tictactoe/src/tictactoe.cc
+++ games/tictactoe/src/tictactoe.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <net/client-messenger.hh>
+#include <net/server-messenger.hh>
 
 extern "C" {
 
@@ -8,7 +9,14 @@ void client_loop(net::ClientMessenger_sptr client_msgr)
 {
     (void) client_msgr;
 
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello Client!" << std::endl;
+}
+
+void server_loop(net::ServerMessenger_sptr server_msgr)
+{
+    (void) server_msgr;
+
+    std::cout << "Hello Server!" << std::endl;
 }
 
 } // extern "C"
