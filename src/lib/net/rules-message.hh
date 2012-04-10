254f905b6490b8ae24a152033450fd9615704290
Nicolas Hureau <nh@kalenz.fr>
2012-04-10 09:16:08 +0200

    net: Add RulesMessage interface

--- src/lib/net/rules-message.hh
+++ src/lib/net/rules-message.hh
@@ -1,8 +1,19 @@
 #ifndef RULES_MESSAGE_HH_
 # define RULES_MESSAGE_HH_
 
+# include <cstdint>
+
+// Rules have to implement this interface for their messages
+
 class RulesMessage
 {
+public:
+    // Size of the whole RulesMessage
+    virtual uint32_t size() const = 0;
+    virtual void size_set(uint32_t) = 0;
+
+    // The RulesMessage in itself
+    virtual void* data() = 0;
 };
 
 #endif // !RULES_MESSAGE_HH_
