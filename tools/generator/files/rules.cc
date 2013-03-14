d99c4eec19ca8afab6227877a5151a08ac2ccdb6
Antoine Pietri <antoine.pietri@epita.fr>
2013-03-14 02:06:07 +0000

    generator: added loops in rules.cc

--- tools/generator/files/rules.cc
+++ tools/generator/files/rules.cc
@@ -14,3 +14,18 @@ Rules::~Rules()
 
     delete champion_dll_;
 }
+
+void Rules::client_loop(rules::ClientMessenger_sptr msgr)
+{
+    // FIXME
+}
+
+void Rules::spectator_loop(rules::ClientMessenger_sptr msgr)
+{
+    // FIXME
+}
+
+void Rules::server_loop(rules::ServerMessenger_sptr msgr)
+{
+    // FIXME
+}
