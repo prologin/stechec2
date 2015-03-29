f459cc9f2d02af9f3486e5bf91203b10d7d61c34
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-03-29 18:27:16 +0200

    change namespace 'google' to 'gflags' for compatibility with gflags 2.1.*

--- src/client/main.cc
+++ src/client/main.cc
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    google::ParseCommandLineFlags(&argc, &argv, true);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     Client client;
     client.run();
--- src/server/main.cc
+++ src/server/main.cc
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    google::ParseCommandLineFlags(&argc, &argv, true);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     // The game is on, Mrs Hudson!
     Server server;
