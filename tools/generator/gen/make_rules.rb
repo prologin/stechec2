7f38f0ed1efe90919ae4afd44a7bb198aa3b5f02
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-11-12 00:16:03 +0100

    generator: make_rules: add synchronisation check in apply_action template

--- tools/generator/gen/make_rules.rb
+++ tools/generator/gen/make_rules.rb
@@ -473,6 +473,16 @@ rules::Actions* Rules::get_actions()
 
 void Rules::apply_action(const rules::IAction_sptr& action)
 {
+    // When receiving an action, the API should have already checked that it
+    // is valid. We recheck that for the current gamestate here to avoid weird
+    // client/server desynchronizations and make sure the gamestate is always
+    // consistent across the clients and the server.
+
+    int err = action->check(api_->game_state());
+    if (err)
+        FATAL("Synchronization error: received action %d from player %d, but "
+              "check() on current gamestate returned %d.",
+              action->id(), action->player_id(), err);
     api_->game_state_set(action->apply(api_->game_state()));
 }
 
