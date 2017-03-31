805bc02788f2366404384500058af537514447d9
Cbeausei <cbeausei@cbeausei.online.net>
2017-03-31 17:04:29 +0200

    tools/generator: cast enum to int for hashes

--- tools/generator/gen/file_generator.rb
+++ tools/generator/gen/file_generator.rb
@@ -429,6 +429,8 @@ class CProto < FileGenerator
   def conv_type(type)
     if type.is_array?
       "std::vector<#{type.type.name}>"
+    elsif type.is_enum?
+      "int"
     else
       type.name
     end
