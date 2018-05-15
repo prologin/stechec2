e816b9d0550a13509943f1494fd2f41942edea38
Malo Jaffré <jaffre.malo@gmail.com>
2018-05-15 18:13:08 +0200

    Wrap the Rust FFI in an idiomatic API
    As discussed in #37.
    
    This doesen't yet eliminate the C
    layer, because it seems very difficult to allocate and construct in C++
    a `Vec<T>` (its layout is unspecified so that its fields can be
    reordered by rustc, and it is incompatible with malloced buffers), but
    all these horrible `Array<T>` are now hidden in a new `ffi` module.
    
    Nevertheless, this greatly simplifies the API usage, as seen in the
    `prologin.rs` test.

--- tools/generator/gen/generator_rust.rb
+++ tools/generator/gen/generator_rust.rb
@@ -12,15 +12,14 @@
 
 require "pathname"
 require "gen/file_generator"
-require "gen/generator_c"
 
-def rust_typename(type)
+def rust_ffi_typename(type)
   if type.name == "string"
     "*const char"
   elsif type.name == "bool"
     "bool"
   elsif type.is_array?
-     "Array<" + rust_typename(type.type) + ">"
+     "Array<" + rust_ffi_typename(type.type) + ">"
   elsif ["int", "float", "double"].include? type.name
     "c_" + type.name
   else
@@ -28,14 +27,135 @@ def rust_typename(type)
   end
 end
 
-def rust_proto(fn)
+def rust_api_typename(type)
+  if type.is_array?
+     "Vec<" + rust_api_typename(type.type) + ">"
+   else
+     rust_ffi_typename(type)
+   end
+end
+
+def rust_ffi_proto(fn)
   # Returns the prototype of a C function
-  args = fn.args.map { |arg| "#{arg.name}: #{rust_typename(arg.type)}" }.join(", ")
-  ret = fn.ret.is_nil? ? "" : " -> #{rust_typename(fn.ret)}"
+  args = fn.args.map { |arg| "#{arg.name}: #{rust_ffi_typename(arg.type)}" }.join(", ")
+  ret = fn.ret.is_nil? ? "" : " -> #{rust_ffi_typename(fn.ret)}"
   "fn #{fn.name}(#{args})" + ret
 end
 
-class CCxxFileGeneratorForRust < CCxxFileGenerator
+def rust_api_proto(fn)
+  # Returns the prototype of a C function
+  args = fn.args.map { |arg| "#{arg.name}: &#{rust_api_typename(arg.type)}" }.join(", ")
+  ret = fn.ret.is_nil? ? "" : " -> #{rust_api_typename(fn.ret)}"
+  "fn #{fn.name}(#{args})" + ret
+end
+
+class RustCxxFileGenerator < CCxxFileGenerator
+  def initialize
+    super
+    @lang = "C++ (for Rust interface)"
+  end
+
+  def generate_source
+    @f = File.open(@path + @source_file, 'w')
+    print_banner "generator_c.rb"
+    @f.puts <<-EOF
+#include "interface.hh"
+#include <cstdlib>
+
+template<typename Lang, typename Cxx>
+Cxx lang2cxx(Lang in)
+{
+  return in;
+}
+
+template<>
+std::string lang2cxx<char*, std::string>(char* in)
+{
+  return in;
+}
+
+template<typename Lang, typename Lang_array, typename Cxx>
+std::vector<Cxx> lang2cxx_array(Lang_array in)
+{
+  std::vector<Cxx> out(in.length);
+  for (size_t i = 0; i < in.length; ++i)
+    out[i] = lang2cxx<Lang, Cxx>(in.datas[i]);
+  free(in.datas);
+  return out;
+}
+
+template<typename Lang, typename Cxx>
+Lang cxx2lang(Cxx in)
+{
+  return in;
+}
+
+template<>
+char* cxx2lang<char*, std::string>(std::string in)
+{
+  size_t l = in.length();
+  char* out = (char *) malloc(l + 1);
+  for (int i = 0; i < l; i++) out[i] = in[i];
+  out[l] = 0;
+  return out;
+}
+
+template<typename Lang, typename Lang_array, typename Cxx>
+Lang_array cxx2lang_array(const std::vector<Cxx>& in)
+{
+  Lang_array out = { NULL, in.size() };
+  out.datas = (Lang *)malloc((out.length) * sizeof(Lang));
+  for (int i = 0; i < out.length; ++i)
+    out.datas[i] = cxx2lang<Lang, Cxx>(in[i]);
+  return out;
+}
+EOF
+
+    for_each_struct do |x|
+      rtype = x['str_name']
+      cxxtype = "__internal__cxx__#{rtype}"
+      @f.puts <<-EOF
+template<>
+#{cxxtype} lang2cxx<#{rtype}, #{cxxtype}>(#{rtype} in)
+{
+  #{cxxtype} out;
+EOF
+      x['str_field'].each do |f|
+        name = f[0]
+        type = @types[f[1]]
+        @f.puts "  out.#{name} = #{get_lang2cxx(type)}(in.#{name});"
+      end
+      @f.puts <<-EOF
+  return out;
+}
+
+template<>
+#{rtype} cxx2lang<#{rtype}, #{cxxtype}>(#{cxxtype} in)
+{
+  #{rtype} out;
+EOF
+      x['str_field'].each do |f|
+        name = f[0]
+        type = @types[f[1]]
+        @f.puts "  out.#{name} = #{get_cxx2lang(type)}(in.#{name});"
+      end
+      @f.puts "  return out;", "}", ""
+    end
+
+    @f.puts <<-EOF
+extern "C" {
+EOF
+    for_each_fun do |fn|
+      @f.puts c_proto(fn)
+      buffer = "api_#{fn.name}("
+      buffer += fn.args.map { |arg| "#{get_lang2cxx(arg.type)}(#{arg.name})" }.join(", ") + ")"
+      buffer = "return #{get_cxx2lang(fn.ret)}(#{buffer})" if not fn.ret.is_nil?
+      @f.puts "{", "  #{buffer};", "}", ""
+    end
+    @f.puts "}"
+    @f.close
+  end
+
   def build
     @path = Pathname.new($install_path) + "rust"
     @source_file = 'interface.cc'
@@ -77,25 +197,43 @@ class RustFileGenerator < FileGenerator
     end
   end
 
-  def build_structs_generic(&show_field)
-    for_each_struct do |x|
-      @f.puts "#[repr(C)]"
-      @f.puts "#[derive(Clone)]"
-      @f.puts "pub struct #{camel_case(x['str_name'])} {"
-      x['str_field'].each do |f|
-        @f.print "  /// ", f[2], "\n"
-        @f.print "  pub ", show_field.call(f[0], f[1]), ", \n"
-      end
-      @f.print "}\n\n"
+  def build_struct_generic(struct, &typename)
+    name = camel_case(struct['str_name'])
+    @f.puts "pub struct #{camel_case(struct['str_name'])} {"
+    struct['str_field'].each do |f|
+      type = @types[f[1]]
+      type = typename.call(type)
+      @f.puts "  /// #{f[2]}"
+      @f.puts "  pub #{f[0]}: #{type},"
     end
+    @f.puts "}"
+    @f.puts
   end
 
-  def generate_api
-    @f = File.open(@path + @api_file, 'w')
+  def impl_conv_id(type)
+    @f.puts "impl ToAPI for #{type} {"
+    @f.puts "  type To = #{type};"
+    @f.puts "  unsafe fn to_api(&self) -> Self::To { self.clone() }"
+    @f.puts "}"
+    @f.puts
+    @f.puts "impl ToFFI for #{type} {"
+    @f.puts "  type To = #{type};"
+    @f.puts "  fn to_ffi(&self) -> Self::To { self.clone() }"
+    @f.puts "}"
+    @f.puts
+  end
+
+  def generate_ffi
+    @f = File.open(@path + @ffi_file, 'w')
     print_banner "generator_rust.rb"
+    @f.puts "use api::{self,"
+    for_each_enum do |x|
+      @f.print "#{camel_case(x['enum_name'])},"
+    end
+    @f.puts "};"
         @f.puts <<-EOF
-#![allow(unused)]
-use std::os::raw::{c_double, c_int, c_void};
+use std::{mem, ptr, slice};
+use std::os::raw::{c_float ,c_double, c_int, c_void};
 
 #[repr(C)]
 #[derive(Clone)]
@@ -104,34 +242,125 @@ pub struct Array<T> {
     pub len: usize,
 }
 
-impl<T> Array<T> {
-    // Doit etre appele exactement une fois par `Array`
-    // retourne par une fonction de l'API.
-    pub unsafe fn free(self) {
-      free(self.ptr as _);
-    }
+pub trait ToAPI {
+  type To;
+  unsafe fn to_api(&self) -> Self::To;
 }
+
+pub trait ToFFI {
+  type To;
+  fn to_ffi(&self) -> Self::To;
+}
+
+impl<T: ToAPI + Clone> ToAPI for Array<T> {
+  type To = Vec<T::To>;
+  unsafe fn to_api(&self) -> Self::To {
+    let vec;
+    {
+      let slice = slice::from_raw_parts(self.ptr, self.len);
+      vec = slice.iter().map(|elem| elem.to_api()).collect();
+    }
+    free(self.ptr as _);
+    vec
+  }
+}
+
+impl<T: ToFFI> ToFFI for Vec<T> {
+  type To = Array<T::To>;
+  fn to_ffi(&self) -> Self::To {
+    unsafe {
+      let ptr: *mut T::To = malloc(self.len() * mem::size_of::<T::To>()) as _;
+      let len = self.len();
+      for i in 0..len {
+        ptr::write(ptr.add(i), self[i].to_ffi());
+      }
+      Array { ptr, len }
+    }
+  }
+}
+
+EOF
+
+    for type in ["()", "c_int", "c_float", "c_double", "bool", "*const char"]
+      impl_conv_id(type)
+    end
+
+    for_each_enum do |x|
+      impl_conv_id(camel_case(x['enum_name']))
+    end
+
+    for_each_struct do |x|
+      name = camel_case(x['str_name'])
+      @f.puts "#[repr(C)]"
+      @f.puts "#[derive(Clone)]"
+      build_struct_generic(x) do |type|
+        rust_ffi_typename(type)
+      end
+
+      @f.puts "impl ToAPI for #{name} {"
+      @f.puts "  type To = api::#{name};"
+      @f.puts "  unsafe fn to_api(&self) -> Self::To {"
+      @f.puts "    api::#{name} {"
+      x['str_field'].each do |f|
+        @f.puts "      #{f[0]}: self.#{f[0]}.to_api(),"
+      end
+      @f.puts "    }"
+      @f.puts "  }"
+      @f.puts "}"
+      @f.puts
+      @f.puts "impl ToFFI for api::#{name} {"
+      @f.puts "  type To = #{name};"
+      @f.puts "  fn to_ffi(&self) -> Self::To {"
+      @f.puts "    #{name} {"
+      x['str_field'].each do |f|
+        @f.puts "      #{f[0]}: self.#{f[0]}.to_ffi(),"
+      end
+      @f.puts "    }"
+      @f.puts "  }"
+      @f.puts "}"
+      @f.puts
+    end
+
+    @f.puts "extern {"
+    @f.puts "fn malloc(size: usize) -> *mut c_void;", ""
+    @f.puts "fn free(p: *mut c_void);", ""
+    for_each_fun do |f|
+      @f.print "pub ", rust_ffi_proto(f), ";\n"
+    end
+    @f.puts "}"
+    @f.close
+  end
+
+def generate_api
+    @f = File.open(@path + @api_file, 'w')
+    @f.puts <<-EOF
+#![allow(unused)]
+use ffi::{self, ToAPI, ToFFI};
+use std::os::raw::{c_float, c_double, c_int, c_void};
+
 EOF
 
     build_constants
     build_enums
 
-    build_structs_generic do |field, type|
-      type = @types[type]
-      "#{field}: #{rust_typename(type)}"
+    for_each_struct do |x|
+      @f.puts "#[derive(Clone, Debug, PartialEq, PartialOrd)]"
+      build_struct_generic(x) do |type|
+        rust_api_typename(type)
+      end
     end
 
-    @f.puts "extern {"
-    @f.puts "fn free(p: *mut c_void);", ""
     for_each_fun do |f|
-      @f.print "pub ", rust_proto(f), ";\n"
+      @f.print "pub ", rust_api_proto(f)
+      args = f.args.map { |arg| "#{arg.name}.to_ffi()" }.join(", ")
+      print_body "  unsafe { ffi::#{f.name}(#{args}).to_api() }"
     end
-    @f.puts "}", ""
+
     for_each_user_fun do |f|
       @f.puts"#[no_mangle]"
-      @f.print "pub unsafe extern \"C\" ", rust_proto(f)
-      args = f.args.map { |arg| arg.name }.join(", ")
-      print_body "  super::#{f.name}(#{args})"
+      @f.print "pub unsafe extern \"C\" ", rust_ffi_proto(f)
+      args = f.args.map { |arg| "&#{arg.name}.to_api()" }.join(", ")
+      print_body "  super::#{f.name}(#{args}).to_ffi()"
     end
     @f.close
   end
@@ -140,10 +369,12 @@ EOF
     @f = File.open(@path + @user_file, 'w')
     @f.puts "#![allow(unused)]"
     @f.puts "pub mod api;"
+    @f.puts "mod ffi;"
+    @f.puts
     @f.puts "use api::*;"
     @f.puts "use std::os::raw::{c_double, c_int, c_void};", ""
     for_each_user_fun do |f|
-      @f.print rust_proto(f)
+      @f.print rust_api_proto(f)
       print_body "  // fonction a completer"
     end
     @f.close
@@ -151,6 +382,7 @@ EOF
 
   def generate_makefile
     target = $conf['conf']['player_lib']
+    header_file = $conf['conf']['player_filename'] + ".h"
     @f = File.open(@path + "Makefile", 'w')
     @f.print <<-EOF
 # -*- Makefile -*-
@@ -162,7 +394,7 @@ lib_TARGETS = #{target}
 #{target}-rustcflags = -g
 
 # Evite de toucher a ce qui suit
-#{target}-dists += #{$conf['conf']['player_filename']}.h interface.hh #{@api_file}
+#{target}-dists += #{header_file} interface.hh #{@ffi_file} #{@api_file}
 #{target}-srcs += interface.cc #{@user_file}
 #{target}-ldflags = -fPIC -Wl,--whole-archive
 #{target}-ldlibs  = -Wl,--no-whole-archive -lm -lrt -ldl -lpthread -lstdc++
@@ -175,10 +407,11 @@ include ../includes/rules.mk
 
 
   def build()
-    CCxxFileGeneratorForRust.new.build
+    RustCxxFileGenerator.new.build
 
     @path = Pathname.new($install_path) + "rust"
     @user_file = $conf['conf']['player_filename'] + '.rs'
+    @ffi_file = 'ffi.rs'
     @api_file = 'api.rs'
 
     header_generator = CFileGenerator.new
@@ -187,6 +420,7 @@ include ../includes/rules.mk
 
     header_generator.generate_header
     generate_user
+    generate_ffi
     generate_api
     generate_makefile
   end
