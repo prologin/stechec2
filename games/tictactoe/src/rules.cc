33512267b2696354b8bad104eeefbc51730a2c5a
haltode <tallancon@gmail.com>
2018-06-01 20:14:03 +0200

    tictactoe: src: rules: implement rules

--- games/tictactoe/src/rules.cc
+++ games/tictactoe/src/rules.cc
@@ -9,7 +9,12 @@ Rules::Rules(const rules::Options opt)
     {
         champion_dll_ = std::make_unique<utils::DLL>(opt.champion_lib);
 
-        // FIXME: register user functions
+        champion_init_game_ =
+            champion_dll_->get<f_champion_init_game>("init_game");
+        champion_play_turn_ =
+            champion_dll_->get<f_champion_play_turn>("play_turn");
+        champion_end_game_ =
+            champion_dll_->get<f_champion_end_game>("end_game");
     }
 
     GameState* game_state = new GameState(opt.players);
@@ -43,8 +48,78 @@ void Rules::apply_action(const rules::IAction_sptr& action)
     api_->game_state_set(action->apply(api_->game_state()));
 }
 
+void Rules::at_player_start(rules::ClientMessenger_sptr)
+{
+    try
+    {
+        sandbox_.execute(champion_init_game_);
+    }
+    catch (utils::SandboxTimeout)
+    {
+        FATAL("player_start: timeout");
+    }
+}
+
+void Rules::at_spectator_start(rules::ClientMessenger_sptr)
+{
+    champion_init_game_();
+}
+
+void Rules::at_player_end(rules::ClientMessenger_sptr)
+{
+    try
+    {
+        sandbox_.execute(champion_end_game_);
+    }
+    catch (utils::SandboxTimeout)
+    {
+        FATAL("player_end: timeout");
+    }
+}
+
+void Rules::at_spectator_end(rules::ClientMessenger_sptr)
+{
+    champion_end_game_();
+}
+
+void Rules::player_turn()
+{
+    try
+    {
+        sandbox_.execute(champion_play_turn_);
+    }
+    catch (utils::SandboxTimeout)
+    {
+        FATAL("player_turn: timeout");
+    }
+}
+
+void Rules::spectator_turn()
+{
+    champion_play_turn_();
+}
+
+void Rules::start_of_player_turn(unsigned int player_id)
+{
+    api_->game_state()->reset_action_points(player_id);
+    api_->game_state()->reset_history(player_id);
+}
+
+void Rules::end_of_player_turn(unsigned int /* player_id */)
+{
+    // Clear the list of game states at the end of each turn (half-round)
+    // We need the linked list of game states only for undo and history,
+    // therefore old states are not needed anymore after the turn ends.
+    api_->game_state()->clear_old_version();
+}
+
 bool Rules::is_finished()
 {
-    // FIXME
-    return true;
+    const GameState* st = api_->game_state();
+    std::vector<int> board = st->get_board();
+    bool is_full = true;
+    for (int cell : board)
+        if (cell == st->NO_PLAYER)
+            is_full = false;
+    return st->winner() != st->NO_PLAYER || is_full;
 }
--- games/tictactoe/src/rules.hh
+++ games/tictactoe/src/rules.hh
@@ -14,6 +14,10 @@
 
 #include "api.hh"
 
+typedef void (*f_champion_init_game)();
+typedef void (*f_champion_play_turn)();
+typedef void (*f_champion_end_game)();
+
 class Rules : public rules::TurnBasedRules
 {
 public:
@@ -24,8 +28,21 @@ public:
     void apply_action(const rules::IAction_sptr& action) override;
     bool is_finished() override;
 
+    void at_player_start(rules::ClientMessenger_sptr) override;
+    void at_spectator_start(rules::ClientMessenger_sptr) override;
+    void at_player_end(rules::ClientMessenger_sptr) override;
+    void at_spectator_end(rules::ClientMessenger_sptr) override;
+
+    void player_turn() override;
+    void spectator_turn() override;
+
+    void start_of_player_turn(unsigned int player_id) override;
+    void end_of_player_turn(unsigned int player_id) override;
+
 protected:
-    // FIXME: Override TurnBasedRules methods here
+    f_champion_init_game champion_init_game_;
+    f_champion_play_turn champion_play_turn_;
+    f_champion_end_game champion_end_game_;
 
 private:
     void register_actions();
