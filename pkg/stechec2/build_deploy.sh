63ff919158c5893fefac9b92241dacb6cc669e2c
Rémi Audebert <rflah0@gmail.com>
2019-05-31 19:17:36 +0200

    pkg: stechec2: add build_deploy.sh

---
+++ pkg/stechec2/build_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+makepkg -f
+all_pkgs=(stechec2-*.pkg.tar.xz)
+latest_pkg=${all_pkgs[-1]}
+scp $latest_pkg root@rhfs01:
+ssh root@rhfs01 -- pacman --noconfirm -U $latest_pkg --root /export/nfsroot_staging
+ssh root@rhfs01 -- /root/sadm/rfs/commit_staging.sh rhfs01 rhfs23 rhfs45 rhfs67 rhfs89
