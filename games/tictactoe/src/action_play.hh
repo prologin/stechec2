e3bb9054c4c1e5348593c9d307d385f8e3e61d42
Rémi Audebert <rflah0@gmail.com>
2019-06-22 17:50:48 +0200

    tictactoe: rules: remove refactor leftovers

--- games/tictactoe/src/action_play.hh
+++ games/tictactoe/src/action_play.hh
@@ -17,8 +17,6 @@ public:
 
     int check(const GameState& st) const override;
     void apply_on(GameState* st) const override;
-    // Unhide base class apply()
-    using rules::Action<GameState>::apply;
 
     void handle_buffer(utils::Buffer& buf) override
     {
