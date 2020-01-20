27cae5a20dac450f198dd224401a7ddcf0b88c74
Sacha Delanoue <contact@shaac.me>
2020-01-20 23:57:21 +0100

    tests with jinja2

---
+++ tools/generator/make_rules.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import jinja2
+import os
+import sys
+import yaml
+from pathlib import Path
+from shutil import copyfile
+
+
+def cxx_type(value: str) -> str:
+    if value == "string":
+        return "std::string"
+    if value.endswith(" array"):
+        return "std::vector<{}>".format(cxx_type(value[:-6]))
+    return value
+
+def cxx_args(value):
+    return ", ".join("{} {}".format(cxx_type(i[1]), i[0]) for i in value)
+
+
+def make_rules(yaml_path: str, install_path: str) -> None:
+    """Generate the template to code a new stechec rules project"""
+
+    conf = yaml.safe_load(open(yaml_path, "r"))
+    # TODO check that conf is valid
+
+    Path(install_path).mkdir(parents=True, exist_ok=True)
+
+    template_folder = "files" # TODO search in PREFIX/share/stechec2 as well
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=template_folder))
+    env.filters["cxx_type"] = cxx_type
+    env.filters["cxx_args"] = cxx_args
+
+    for copy in ["entry.cc", "game_state.cc", "game_state.hh"]:
+        copyfile(os.path.join(template_folder, copy), os.path.join(install_path, copy))
+
+    for gen in ["actions.hh", "api.cc", "api.hh", "constant.hh", "interface.cc", "rules.hh", "rules.cc"]:
+        env.get_template(gen + ".jinja2").stream(yaml=conf).dump(os.path.join(install_path, gen))
+
+    for action in conf["function"]:
+        if "fct_action" in action and action["fct_action"]:
+            env.get_template("action_template.hh.jinja2").stream(func=action).dump(os.path.join(install_path, "action_") + action["fct_name"] + ".hh")
+            env.get_template("action_template.cc.jinja2").stream(func=action).dump(os.path.join(install_path, "action_") + action["fct_name"] + ".cc")
+
+if __name__ == "__main__":
+    make_rules(sys.argv[1], sys.argv[2])
