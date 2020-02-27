20d24311751e340c332561b43167c58d5a51d449
Antoine Pietri <antoine.pietri1@gmail.com>
2020-02-27 15:21:03 +0100

    generator: tests: exit on failure

--- tools/generator/test/__main__.py
+++ tools/generator/test/__main__.py
@@ -1,6 +1,8 @@
 import os
 import unittest
+import sys
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().discover(os.path.dirname(__file__))
     result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(not result.wasSuccessful())
