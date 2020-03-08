e8cda9a74ddf667e9d7a9738f00fc490335f40c4
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-08 10:05:23 +0100

    generator: haskell: use get_array_inner filter

--- tools/generator/filters/haskell.py
+++ tools/generator/filters/haskell.py
@@ -1,13 +1,13 @@
 from functools import partial
 
 from . import register_filter
-from .common import generic_comment, is_array
+from .common import generic_comment, get_array_inner, is_array
 
 
 @register_filter
 def haskell_type(type_id: str) -> str:
     if is_array(type_id):
-        return '[' + haskell_type(' '.join(type_id.split()[:-1])) + ']'
+        return '[' + haskell_type(get_array_inner(type_id)) + ']'
     elif type_id == 'void':
         return '()'
     return type_id.capitalize()
