81dc163611c91ace9014caa0021510bae715b048
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-10 20:51:22 +0100

    generator: haskell: fix indentation and array inner in CApi.hsc generation

--- tools/generator/filters/haskell.py
+++ tools/generator/filters/haskell.py
@@ -14,15 +14,36 @@ def haskell_type(type_id: str) -> str:
 
 
 @register_filter
-def c_haskell_type(type_id: str) -> str:
-    def recurse_inner(type_id: str) -> str:
-        if is_array(type_id):
-            return f'{recurse_inner(get_array_inner(type_id))}_array'
-        return type_id.capitalize()
-    return f'C{recurse_inner(type_id)}'
+def haskell_c_type(type_id: str) -> str:
+    if is_array(type_id):
+        return f'{haskell_c_type(get_array_inner(type_id))}_array'
+    return type_id.capitalize()
 
 
 haskell_comment = register_filter(
     partial(generic_comment, start='-- '),
     name='haskell_comment',
 )
+
+
+@register_filter
+def haskell_get_array_types(game: dict):
+    types = list()
+
+    def add_array_type(types, type_id):
+        if is_array(type_id):
+            types.append((haskell_c_type(type_id), type_id))
+            add_array_type(types, get_array_inner(type_id))
+        return
+
+    for s in game['struct']:
+        for _, type_id, _ in s['str_field']:
+            add_array_type(types, type_id)
+
+    for f in game['function'] + game['user_function']:
+        add_array_type(types, f['fct_ret_type'])
+        for arg in f['fct_arg']:
+            add_array_type(types, arg[1])
+
+    types = list(dict.fromkeys(types))
+    return types
