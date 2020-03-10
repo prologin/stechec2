41d5a3bc06a4c25562425c4879cad789ed714a26
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-10 17:22:10 +0100

    generator: haskell: generate structs in CApi.hsc

--- tools/generator/filters/haskell.py
+++ tools/generator/filters/haskell.py
@@ -7,10 +7,19 @@ from .common import generic_comment, get_array_inner, is_array
 @register_filter
 def haskell_type(type_id: str) -> str:
     if is_array(type_id):
-        return '[' + haskell_type(get_array_inner(type_id)) + ']'
+        return f'[{haskell_type(get_array_inner(type_id))}]'
     elif type_id == 'void':
         return '()'
     return type_id.capitalize()
+
+
+@register_filter
+def c_haskell_type(type_id: str) -> str:
+    def recurse_inner(type_id: str) -> str:
+        if is_array(type_id):
+            return f'{recurse_inner(get_array_inner(type_id))}_array'
+        return type_id.capitalize()
+    return f'C{recurse_inner(type_id)}'
 
 
 haskell_comment = register_filter(
