f7158a475664492cc9ff6d32733d3cea22ffb4ea
Antoine Pietri <antoine.pietri1@gmail.com>
2020-04-06 21:22:47 +0200

    generator: C#: fix conversion of arrays in structs
    Fix #117
    
    Co-authored-by: Melyodas <matthieu.moatti@epita.fr>

--- tools/generator/test/languages/champion.cs
+++ tools/generator/test/languages/champion.cs
@@ -89,9 +89,6 @@ namespace Champion {
 
       l = Api.SendMeStructArray(l);
 
-      // FIXME: (XFAIL) FieldIntArr and FieldStrArr are corrupted and assert.
-      // Array handling currently does not work properly for C#.
-      /*
       Assert(l.Length == 42);
       for (int i = 0; i < 42; ++i)
       {
@@ -106,7 +103,6 @@ namespace Champion {
           Assert(l[i].FieldStrArr[j].FieldDouble == 42.42);
         }
       }
-      */
     }
   }
 }
