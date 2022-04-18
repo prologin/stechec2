1252069a4dda0a1c18c6bbff70557d6f6dcd0de4
Léo Portemont <leo.portemont@prologin.org>
2022-04-18 13:02:33 +0200

    nix: add plusminus example game

--- flake.nix
+++ flake.nix
@@ -15,7 +15,9 @@
       rec {
         packages = futils.lib.flattenTree {
           stechec2 = import ./nix/stechec2.nix { inherit pkgs lib; };
-          tictactoe = import ./nix/tictactoe.nix { inherit pkgs lib; stechec2 = packages.stechec2; };
+
+          tictactoe = import ./nix/tictactoe.nix { inherit pkgs lib; inherit (packages) stechec2; };
+          plusminus = import ./nix/plusminus.nix { inherit pkgs lib; inherit (packages) stechec2; };
         };
 
         defaultPackage = packages.stechec2;
---
+++ nix/plusminus.nix
@@ -0,0 +1,8 @@
+{ lib, pkgs, stechec2, ... }:
+
+lib.mkStechec2Game {
+  name = "plusminus";
+  game = ../games/plusminus;
+  stechec2 = stechec2;
+  version = "1.0";
+}
